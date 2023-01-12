54f7b861dc7eb4ad4f38a3bc19328bb91a19c38f
Rajan Khade <rajankhade31@gmail.com>
2023-01-12 15:06:59 +0530

    update article list description

--- components/Description.js
+++ components/Description.js
@@ -1,10 +1,7 @@
 const Description = () => {
 	return (
 		<div className='text-xl text-center text-gray-500 dark:text-gray-400'>
-			There are many variations of passages of Lorem Ipsum available, but
-			the majority have suffered alteration in some form, by injected
-			humour, or randomised words which don&apos;t look even slightly
-			believable.
+			This is a list of some of my technical articles. It involves newly learned technologies, tutorials, installation guides, etc.
 		</div>
 	)
 }
